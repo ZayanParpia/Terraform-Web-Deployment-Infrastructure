f74de7f6a1d766297b78de97b0b39e4717497ff8
ZayanParpia <zparpia2009@gmail.com>
2026-09-16 15:53:40 -0400

    Got ALB to distrubute traffic when the ec2 instances have public ip

--- network.tf
+++ network.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "Terraform_Web_Subnet_A" {
   vpc_id            = aws_vpc.Terraform_Web_Vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  #map_public_ip_on_launch = "true"
 
   tags = {
     Name = "Subnet A"
@@ -23,6 +24,7 @@ resource "aws_subnet" "Terraform_Web_Subnet_B" {
   vpc_id            = aws_vpc.Terraform_Web_Vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  #map_public_ip_on_launch = "true"
 
   tags = {
     Name = "Subnet B"
